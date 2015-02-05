4d234a96c41e194b07c473ebd4f7e267be977983
lobo <jochenbartl@mailbox.org>
2015-02-05 23:29:51 +0100

    Some more initial cleanup foo

---
+++ README.rst
@@ -0,0 +1,24 @@
+netcliparsers - Parser library for screen scraping CLI output from network devices
+==================================================================================
+
+This will become a library of parsers based on `pyparsing <http://pyparsing.wikispaces.com/>`_ for parsing screen scraped CLI text output from network devices.
+
+
+Usage Examples
+--------------
+
+soon (tm)
+
+
+Windows
+-------
+
+If you run the setup on Windows you'll need to have git installed. Pbr tries to figure out the version number via git. I'll need to fix this "soon".
+
+
+Feedback
+--------
+
+Bug reports, patches and ideas are welcome.
+
+Just send me an e-mail (jochenbartl@mailbox.org) or open an issue on GitHub
--- netcliparsers/cisco/ios.py
+++ netcliparsers/cisco/ios.py
@@ -35,3 +35,29 @@ def show_class_map():
     return parser
 
 
+def show_cdp_neighbor_detail():
+    hostname = Word(alphanums, bodyChars=alphanums + '_-.')
+    deviceid = Suppress(Keyword('Device ID:')) + hostname('deviceid')
+
+    ipaddrs = Suppress(Keyword('IP address:')) + Combine(Word(nums) + '.' + Word(nums) + '.' + Word(nums) + '.' + Word(nums))
+    # FIXME last match overwrites first address
+    entryaddrs = Suppress(Keyword('Entry address(es):')) + OneOrMore(ipaddrs('ipaddress'))
+
+    platform = Word(alphanums, bodyChars=alphanums + '- ') 
+    # FIXME Strip whitespace off string end
+    capabilities = Word(alphanums, bodyChars=alphanums + '- ') 
+    platformcapabilities = Suppress(Keyword('Platform:')) + platform('platform') + Suppress(',') +\
+                           Keyword('Capabilities:') + capabilities('capabilities')
+
+    interface = Word(alphas, bodyChars=alphanums + '/.')
+    interfaces = Suppress(Keyword('Interface:')) + interface('localinterface') + Suppress(',') +\
+                 Suppress(Keyword('Port ID (outgoing port):')) + interface('remoteinterface')
+
+    holdtimenum = Word(nums).setParseAction(lambda tokens: int(tokens[0]))
+    holdtime = Suppress('Holdtime') + Suppress(':') + holdtimenum('holdtime') + Suppress('sec')
+
+    cdpentry = deviceid + entryaddrs + platformcapabilities + interfaces + holdtime
+
+    parser = OneOrMore(cdpentry)
+
+    return parser
